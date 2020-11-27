8eab9d7af99cba073400519dfd50dc4f16334a05
Edouard Murat <edouardmurat1@gmail.com>
2020-11-27 22:50:51 +0000

    Center camera to current rabbit

--- Camera.gd
+++ Camera.gd
@@ -1,16 +1,15 @@
 extends Camera
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+var center_translation
+var center_tile_coords
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	center_translation = translation
+	center_tile_coords = Vector3(10, 0, 10)
 
-
-func move_to(x, z):
-	$Tween.interpolate_property(self, "translation", translation, Vector3(x, translation.y, z), 1.0, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+func move_to(w, h):
+	var new_pos = center_translation - (center_tile_coords - Vector3(w,0,h))
+	
+	$Tween.interpolate_property(self, "translation", translation, new_pos, 0.5, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 	$Tween.start()
